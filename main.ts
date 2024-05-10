6f9add6969efec08ccbb0a7b327984f9a976f7d9
unknown <seherqureshi0602@gamil.com>
2024-05-10 09:00:41 -0700

    twit the text

--- main.ts
+++ main.ts
@@ -3,8 +3,8 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
-  { message: "enter second number", type: "number", name: "secondNumber" },
+  { message: "Enter your first number", type: "number", name: "firstNumber" },
+  { message: "Enter your second number", type: "number", name: "secondNumber" },
   {
     message: "select one of the operater to perform action",
     type: "list",
